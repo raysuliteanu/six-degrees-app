34cca0395e345c1c8b51fd35e3943215da959f26
Ray Suliteanu <raysuliteanu@pm.me>
2025-12-26 15:38:56 -0800

    initial project from start.spring.io plus some organization

---
+++ server/src/main/java/org/kidoni/sixdegrees/SixDegreesAppApplication.java
@@ -0,0 +1,13 @@
+package org.kidoni.sixdegrees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixDegreesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixDegreesAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/kidoni/sixdegrees/SixDegreesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kidoni.sixdegrees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixDegreesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
