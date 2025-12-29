d1283bca1d14539cec65a7c8589fafb08fb12886
Ray Suliteanu <raysuliteanu@pm.me>
2025-12-28 16:01:45 -0800

    task: start pattern of getting person/movie details after search
    fork threads to get the movie/person details after a movie/person search
    and return search results immediately, so user is not waiting for the 
    details to be retrieved.

--- server/src/main/java/org/kidoni/sixdegrees/SixDegreesController.java
+++ server/src/main/java/org/kidoni/sixdegrees/SixDegreesController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+// TODO: refactor business logic into a service e.g. how personSearch() is doing several things,
+// it should be done in the service; the controller should only deal with the web request aspects.
+
 @RestController
 public class SixDegreesController {
     private final TmdbClient tmdbClient;
@@ -21,11 +24,21 @@ public class SixDegreesController {
 
     @GetMapping(path = "/search/person/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonSearchResult personSearch(@PathVariable final String name) {
-        return  tmdbClient.searchPersonByName(name);
+        final var searchResult = tmdbClient.searchPersonByName(name);
+        if (searchResult.getResults() != null) {
+            for (final var person : searchResult.getResults()) {
+                Thread.ofVirtual().start(() -> {
+                   var details = tmdbClient.findPersonById(person.getId());
+                   // todo: save details in db
+                });
+            }
+        }
+        return searchResult;
     }
 
     @GetMapping(path = "/person/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonDetails findPerson(@PathVariable final int id) {
+        // todo: look up in db first
         return  tmdbClient.findPersonById(id);
     }
 
@@ -36,12 +49,22 @@ public class SixDegreesController {
 
     @GetMapping(path = "/search/movie/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
     public MovieSearchResult movieSearch(@PathVariable final String name) {
-        return  tmdbClient.searchMovieByName(name);
+        final var searchResult = tmdbClient.searchMovieByName(name);
+        if (searchResult.getResults() != null) {
+            for (final var movie : searchResult.getResults()) {
+                Thread.ofVirtual().start(() -> {
+                    var details = tmdbClient.findMovieById(movie.getId());
+                    // todo: save details in db
+                });
+            }
+        }
+        return searchResult;
     }
 
     @GetMapping(path = "/movie/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public MovieDetails findMovie(@PathVariable final int id) {
-        return  tmdbClient.findMovieById(id);
+        // todo: look up in db first
+        return tmdbClient.findMovieById(id);
     }
 
 }
