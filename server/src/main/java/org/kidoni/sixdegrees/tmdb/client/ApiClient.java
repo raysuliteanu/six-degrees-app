3d8add012005287b6150a42dd7c26313dfc06fd6
Ray Suliteanu <raysuliteanu@pm.me>
2026-01-02 14:51:35 -0800

    task: put back openapi generated code as sourceset

---
+++ server/src/main/java/org/kidoni/sixdegrees/tmdb/client/ApiClient.java
@@ -0,0 +1,33 @@
+package org.kidoni.sixdegrees.tmdb.client;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Minimal utility stub for OpenAPI-generated models.
+ * <p>
+ * The OpenAPI Generator creates model classes that use this utility
+ * for URL query parameter serialization. This stub provides the minimal
+ * implementation needed by the generated code.
+ * <p>
+ * Note: Only valueToString() is actually used (3008+ times across models).
+ * We don't generate API clients - those are manually implemented.
+ */
+public abstract class ApiClient {
+    /**
+     * Convert the given value to a string suitable for use as a URL query parameter value.
+     * Used by generated model classes for serializing fields to query strings.
+     *
+     * @param value the value to convert
+     * @return string representation suitable for URL encoding, or empty string if null
+     */
+    public static String valueToString(Object value) {
+        return switch (value) {
+            case null -> "";
+            case OffsetDateTime offsetDateTime -> offsetDateTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            case LocalDate localDate -> localDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+            default -> value.toString();
+        };
+    }
+}
