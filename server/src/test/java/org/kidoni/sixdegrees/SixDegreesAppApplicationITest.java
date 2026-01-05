17e7453475414d5b364662280a3b7239932285d9
Ray Suliteanu <raysuliteanu@pm.me>
2026-01-05 14:03:04 -0800

    fix: integration test needed config properties set

--- server/src/test/java/org/kidoni/sixdegrees/SixDegreesAppApplicationITest.java
+++ server/src/test/java/org/kidoni/sixdegrees/SixDegreesAppApplicationITest.java
@@ -2,9 +2,14 @@ package org.kidoni.sixdegrees;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class SixDegreesAppApplicationITest {
+@TestPropertySource(properties = {
+    "tmdb.api.url=https://api.themoviedb.org/",
+    "tmdb.api.access-token=test-token"
+})
+class SixDegreesAppApplicationITest implements SixDegreesTestContainers {
 
     @Test
     void contextLoads() {
